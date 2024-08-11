cffaf5a1432f2be1d2e6d3fcf83e7975425b2464
Peter Erbeni <86953639+ProggersValentino@users.noreply.github.com>
2024-08-11 20:49:55 +1000

    RTS controls + packages

---
+++ IGB200TortalSquad/Assets/Scripts/Interaction/ActionEvents/MicroTask.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicroTask : MonoBehaviour
+{
+    public float totalTimeToComplete; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //TODO: we want a function that when an object is clicked on will come up with options
+
+
+    /// <summary>
+    /// when the player has completed a task we need to process the end of it
+    /// </summary>
+    public void ProcessTaskCompletion()
+    {
+        //decrease difficulty
+        
+        //destroy object 
+    }
+}
